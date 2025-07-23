9afbd717fbfc8db3e5b392a7f0cf959230be76e8
Yuxun Guo <youuungx@gmail.com>
2025-07-22 19:27:46 -0700

    Merge with the old DVCS data

--- Raw_Data/DVCS_data/DVCS_Data_Proc.py
+++ Raw_Data/DVCS_data/DVCS_Data_Proc.py
@@ -16,7 +16,7 @@ DVCS_xsec_comb = pd.concat([DVCS_xsec_unp, DVCS_xsec_pol], ignore_index=True)
 DVCS_xsec_comb.to_csv(os.path.join(dir_path,"DVCSxsec_New.csv"),index=None)
 DVCS_asymmetry.to_csv(os.path.join(dir_path,"DVCSAsym.csv"),index=None)
 
-DVCS_xsec_old = pd.read_csv(os.path.join(dir_path,'Raw_Data/DVCS_data/DVCSxsec_Old.csv'),names = ['y', 'xB', 't', 'Q', 'phi', 'f', 'delta f', 'pol'])
+DVCS_xsec_old = pd.read_csv(os.path.join(dir_path,'DVCSxsec_Old.csv'),names = ['y', 'xB', 't', 'Q', 'phi', 'f', 'delta f', 'pol'])
 
 def drop_duplicates_rel_tol(df, compare_cols, rtol=1e-2):
     # Separate columns by type
@@ -61,9 +61,6 @@ def drop_duplicates_rel_tol(df, compare_cols, rtol=1e-2):
 combined = pd.concat([DVCS_xsec_comb, DVCS_xsec_old], ignore_index=True)
 combined_sel = combined[(combined['Q'] > Q_threshold) & (combined['xB'] < xB_Cut) & (combined['t']*(combined['xB']-1) - M ** 2 * combined['xB'] ** 2 > 0)]
 
-lengtot = combined.shape[0]
-lengsel = combined_sel.shape[0]
-
 rt = 0.05
 cols_to_check = ['y', 'xB', 't', 'Q', 'phi', 'f', 'pol']
 combined_uni = drop_duplicates_rel_tol(combined, compare_cols=cols_to_check, rtol=rt)
@@ -74,9 +71,10 @@ combined_uni.to_csv(os.path.join(dir_path,'DVCSxsec_Merge.csv'), index=False)
 # Below test the choice of rtol
 # ======================================================
 
-import matplotlib.pyplot as plt
-
 '''
+lengtot = combined.shape[0]
+lengsel = combined_sel.shape[0]
+
 def Test_rtol(rt: float):
     
     combined_uni = drop_duplicates_rel_tol(combined, compare_cols=cols_to_check, rtol=rt)
@@ -97,6 +95,8 @@ dftest = pd.DataFrame(Test, columns=['rtol', 'dup', 'dup2'])
 dftest.to_csv(os.path.join(dir_path,'DVCS_duplicate.csv'), index=False)
 '''
 
+'''
+import matplotlib.pyplot as plt
 
 dftest = pd.read_csv(os.path.join(dir_path,'DVCS_duplicate.csv'),header=0, names=['rtol', 'dup', 'dup2'])
 
@@ -110,3 +110,4 @@ plt.yscale('log')
 plt.legend()
 plt.grid(True)
 plt.show()
+'''
